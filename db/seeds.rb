82b44bf4c286cc5bc801d62a497ea47595b93376
Dan Vernon <danvernon@gmail.com>
2019-02-22 15:54:33 +0000

    updated seed file with hilarious cocktail names

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,24 @@ require 'json'
 
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each_with_index { |k,v| Ingredient.create(name: k["strIngredient1"]) }
 
+cocktail_names = [
+  'Tequila Mockingbird',
+  'Mastering Margarita',
+  'The Old Man and the Seagram’s',
+  'Bridget Jones’s Daiquiri',
+  'The Last of the Mojitos',
+  'The Rye in the Catcher',
+  'Infinite Zest',
+  'Whiskey Business',
+  'The Picture Of Dorian Gray Goose',
+  'Mad Mix: Fury Road']
+
+Dose.destroy_all
 Ingredient.destroy_all
 Cocktail.destroy_all
-Dose.destroy_all
 
-ingredients["drinks"].each_with_index { |k,v| Ingredient.create(name: k["strIngredient1"]) }
+cocktail_names.each do |cocktail|
+  Cocktail.create(name: cocktail)
+end
