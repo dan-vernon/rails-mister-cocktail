87f044cc71fa9ebef31b10ae51efde018698b05b
Dan Vernon <danvernon@gmail.com>
2019-02-22 11:46:05 +0000

    updated routes to create root redirec to cocktails#index, fixed resulting dose_destroy path route in show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
-  get 'doses/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
-    resources :doses, only: [:new, :create, :destroy]
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:show, :new, :create, :destroy] do
+    resources :doses, only: [:new, :create]
   end
+  resources :doses, only: [:destroy]
 end
