00555eebf068ab559f05340590f0bb91ca8f33db
Dan Vernon <danvernon@gmail.com>
2019-02-23 08:20:01 +0000

    added forest admin dashboard gem / config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount ForestLiana::Engine => '/forest'
   root to: 'cocktails#index'
   resources :cocktails, only: [:show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
